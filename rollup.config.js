f2ba3d1233b11804127f6357e6f88db978cb689d
atomita <sleeping.cait.sith+gh@gmail.com>
2018-04-05 14:46:22 +0900

    fix: Output name

--- rollup.config.js
+++ rollup.config.js
@@ -5,7 +5,7 @@ export default {
   output: {
     file: 'dist/deep-get.js',
     format: 'umd',
-    name: 'flattenObject',
+    name: 'optimizatedDeepGet',
     sourceMap: true,
   },
   plugins: [typescript()],
